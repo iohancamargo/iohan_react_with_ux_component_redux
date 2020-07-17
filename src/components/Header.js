bb21d550b6207ad3efaa9147c2b818d714df6cf1
unknown <iohanutfpr@gmail.com>
2020-07-17 17:29:08 -0300

    Ajustasndo page home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 /* Libs  */
-
 import React from 'react';
 import { withRouter } from "react-router";
+
+/* Components */
+import Grid from '@material-ui/core/Grid';
+import CheckIcon from '@material-ui/icons/Check';
 
 export const Header = () => {
     return (
@@ -14,11 +17,41 @@ export const Header = () => {
                             <img src="/images/img_header_left.svg" alt="Hostgator" />
                         </div>
                         <div>
-                            <h1 className="header__title_first">Hospedagem de sistes</h1>
-                            <h1 className="header__title_first">Tenha uma hospedagem de sites estável e evite perder visitantes diariamente</h1>
-                            <h1 className="header__title_first">99,9% de disponibilidade: seu site sempre no ar</h1>
-                            <h1 className="header__title_first">Suporte 24h, todos os dias</h1>
-                            <h1 className="header__title_first">Painel de Controle cPanel</h1>
+                            {/* 
+                            
+                            <p className="header__title_small"><CheckIcon />99,9% de disponibilidade: seu site sempre no ar</p>
+                            <p className="header__title_small"><CheckIcon />Painel de Controle cPanel</p> */}
+                            <Grid container spacing={1} justify="center">
+                                <Grid xs={12} sm={12} md={12} item >
+                                    <p className="header__title_small">Hospedagem de sistes</p>
+                                </Grid>
+                                <Grid xs={12} sm={12} md={12} item>
+                                    <p className="header__title_big">Tenha uma hospedagem de sites estável e evite perder visitantes diariamente</p>
+                                </Grid>
+                                <Grid xs={12} sm={12} md={12} item >
+                                    <p className="header__title_small"><CheckIcon className="header__icon"/> 99,9% de disponibilidade: seu site sempre no ar</p>
+                                </Grid>
+                                
+                                <Grid xs={12} sm={12} md={12} item >
+                                    <p className="header__title_small"><CheckIcon className="header__icon"/> Suporte 24h, todos os dias <CheckIcon className="header__icon"/> Painel de Controle cPanel</p>
+                                </Grid>
+                                
+                                <Grid xs={12} sm={12} md={12} item >
+                                    <img src="/images/img_arrow_down.svg" className="header__img_arrow" alt="Hostgator" />
+                                </Grid>
+                                
+                                
+
+                                {/* <Grid xs={12} sm={12} md={6} lg={6}>
+                                    <p className="header__title_small"><CheckIcon className="header__icon"/> Suporte 24h, todos os dias</p>
+                                    </Grid>
+                                    <Grid xs={12} sm={12} md={6} lg={6}>
+                                    <p className="header__title_small"><CheckIcon className="header__icon"/> Painel de Controle cPanel</p>
+                                </Grid> */}
+                                
+                            </Grid>
+                                
+                            
                         </div>
                         <div className="header__right">
                             <img src="/images/img_header_right.svg" alt="Hostgator" />
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,37 @@
 /* Libs */
 import { withRouter } from "react-router";
-import React from "react";
+import React, { useState } from "react";
 
 /* Components */
+import Grid from '@material-ui/core/Grid';
 import Header from '../components/Header';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import Radio from '@material-ui/core/Radio';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
 
+const styles = {
+    root: {
+      color: green[600],
+      '&$checked': {
+        color: green[500],
+      },
+    },
+    checked: {},
+  };
 const HomePage = () => {
 
-    // const [email, setEmail] = useState('iohan.camargo@vamilly.com.br');    
+    const [selectedValue, setSelectedValue] = useState('a');    
+
     // const [password, setPassword] = useState('123456');
+
     
+    const handleChange = () => {
+        setSelectedValue('b')
+    };
+
     // const onSubmit = (e) => {
         
         // e.preventDefault();
@@ -41,7 +63,43 @@ const HomePage = () => {
         <Header />
         <div className="box-layout">            
             <div className="box-layout__box">
-                <h5 className="box-layout__title">Quero pagar a cada</h5>
+                <p className="home__small_title">Quero pagar a cada:</p>
+                
+                <div className="home__group-radio">
+                    <div className="header__left">
+                        <Radio
+                            checked={selectedValue === 'a'}
+                            onChange={handleChange}
+                            value="a"
+                            name="radio-button-demo"
+                            aria-label="A"
+                        /> <span>3 anos</span>
+                    </div>
+
+                    <div>
+                        <Radio
+                            checked={selectedValue === 'b'}
+                            onChange={handleChange}
+                            value="b"
+                            name="radio-button-demo"
+                            aria-label="B"
+                        /> <span>1 ano</span>
+                            
+                        
+                    </div>
+
+                    <div className="header__right">
+                        <Radio
+                            checked={selectedValue === 'c'}
+                            onChange={handleChange}
+                            value="c"
+                            name="radio-button-demo"
+                            aria-label="C"
+                        /><span>1 mes</span>
+                    </div>
+                </div>
+
+                
                 {/* <p>Insira seu e-mail ou login para acessar.</p>
                 <form className="form" onSubmit={onSubmit}>
                     <input value={email} onChange={(e) => setEmail(e.target.value)} />
